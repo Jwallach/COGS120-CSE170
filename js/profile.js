3e139da1ea67a0ee789ac33790202544d1dba1e8
Kevin <k5mao@ucsd.edu>
2018-11-08 00:42:15 -0800

    Added ref links from the mappings

--- js/profile.js
+++ js/profile.js
@@ -1,19 +1,55 @@
 $( document ).ready(function() {
+    //Game mapping
+    var gameMap = games;
+    
+    
     //if not defined,set wishlist to null 
     if (localStorage.getItem("wishlist") === null)
     {
         localStorage.setItem("wishlist",JSON.stringify([]));
     }
     var wishlist = JSON.parse(localStorage.getItem("wishlist"));
-    
     if (wishlist.length == 0)
     {
-        wishlist.push("Your wish list is empty, go to Preferences to add some!");
+        document.getElementById("wishlist_games").innerHTML += "<h5>Your wish list is empty, go to a game's page to add it!<h5>\n";
     }
-    
-    for (var x = 0; x < wishlist.length; x ++)
+    else
     {
-      document.getElementById("wishlist_games").innerHTML += "<h5>" + wishlist[x] + "</h5>\n"; 
+        
+      for (var x = 0; x < wishlist.length; x ++)
+      {
+        document.getElementById("wishlist_games").innerHTML += "<h5><a href=" + gameMap[wishlist[x]] + ">" + wishlist[x] + "</a></h5>\n"; 
+      }
     }
     
-    });
+    
+    //if not defined,set the preferences to null
+    if (localStorage.getItem("preferences") === null)
+    {
+        localStorage.setItem("preferences",JSON.stringify([]));
+    }
+    var pref = JSON.parse(localStorage.getItem("preferences"));
+    if (pref.length == 0)
+    {
+        document.getElementById("aspect_preferences").innerHTML += "<h5>" + "You have no aspect preferences, go to Preferences to add some" +"</h5>\n";
+    }
+    else
+    {
+      
+      for (var x = 0; x < pref.length; x ++)
+      {
+        var like = "";
+        if (pref[x][1] === "like")
+        {
+          like = "Liked";
+        }
+        else
+        {
+          like = "Disliked";
+        }
+        document.getElementById("aspect_preferences").innerHTML += "<h5>" + pref[x][0] + " | " + like + "</h5>\n"; 
+      }
+    }
+    
+
+});
