6cad1ee9ca12834205203f6fc0cd4266066285fd
Kevin <k5mao@ucsd.edu>
2018-11-15 03:06:27 -0800

    All browse pages are now dynamic

---
+++ js/browse_aspects.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    //Game mapping
+    var aspectMap = aspects;
+    
+    for (key in aspectMap)
+    {
+        document.getElementById("aspect_list").innerHTML += "<li><a href=" + aspectMap[key]["link"] + ">" + aspectMap[key]["title"] + "</a></li>";
+    }
+});
