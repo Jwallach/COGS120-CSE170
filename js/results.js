200f973b1f85b2a422a4eb9b62be77f52bc39ffd
Jwallach <jw.wallach@sbcglobal.net>
2018-11-15 03:17:13 -0800

    added categories and aspects to results

--- js/results.js
+++ js/results.js
@@ -4,36 +4,55 @@ $( document ).ready(function() {
     console.log(searchtext);
 
 	searchtext = searchtext.toLowerCase();
-	var title = "";
+	var results = [];
 
-	if (searchtext.includes("over") || searchtext.includes("watch"))
+	
+	for (var g in games)
 	{
-		title = "overwatch";
+		if (games[g]['title'].toLowerCase().includes(searchtext) && searchtext != "")
+        {
+			//Add it to the results
+			results.push(g);
+			document.getElementById("game_result").innerHTML += "<div class='row'><li><h3><a href="+games[g]['link']+"><img class='logo' src="+games[g]['images'][0]+"></img>"+games[g]['title']+"</a></h3>";
+			document.getElementById("game_result").innerHTML += "<div class='desc'>"+games[g]['description']+"</div><br>";
+			document.getElementById("game_result").innerHTML += "<div class='tags'>";
+			for (var cat in games[g]['categories'])
+			{
+				document.getElementById("game_result").innerHTML += "<a href='categories/"+games[g]['categories'][cat]+".html'>("+games[g]['categories'][cat]+")</a> ";
+			}
+			for (var asp in games[g]['aspects'])
+			{
+				document.getElementById("game_result").innerHTML += "<a href='aspects/"+games[g]['aspects'][asp]+".html'>["+games[g]['aspects'][asp]+"]</a> ";
+			}
+			document.getElementById("game_result").innerHTML += "</div></li><br><br></div>";
+        }
 	}
-	else if (searchtext.includes("skyrim") || searchtext.includes("elder scrolls"))
+	for (var c in categories)
 	{
-		title = "skyrim";
+		if (categories[c]['title'].toLowerCase().includes(searchtext) && searchtext != "")
+		{
+			//Add it to the results
+			results.push(c);
+			document.getElementById("cat_result").innerHTML += "<div class='row'><li><h3><a href="+categories[c]['link']+">"+categories[c]['title']+"</a></h3>";
+			document.getElementById("cat_result").innerHTML += "<div class='desc'>"+categories[c]['description']+"</div><br>";
+			document.getElementById("cat_result").innerHTML += "</li><br><br></div>";
+		}
+	}
+	for (var a in aspects)
+	{
+		if (aspects[a]['title'].toLowerCase().includes(searchtext) && searchtext != "")
+		{
+			//Add it to the results
+			results.push(c);
+			document.getElementById("asp_result").innerHTML += "<div class='row'><li><h3><a href="+aspects[a]['link']+">"+aspects[a]['title']+"</a></h3>";
+			document.getElementById("asp_result").innerHTML += "<div class='desc'>"+aspects[a]['description']+"</div><br>";
+			document.getElementById("asp_result").innerHTML += "</li><br><br></div>";
+		}
 	}
 	
-	if (title == "")
+	if (results.length == 0)
 	{
 		document.getElementById("result").innerHTML += "<h1>No results found<h1>\n";
-	}
-	else
-	{
-	
-		document.getElementById("result").innerHTML += "<li><h3><a href="+games[title]['link']+"><img class='logo' src="+games[title]['images'][0]+"></img>"+games[title]['title']+"</a></h3>";
-		document.getElementById("result").innerHTML += "<div class='desc'>"+games[title]['description']+"</div><br>";
-		document.getElementById("result").innerHTML += "<div class='tags'>";
-		for (var cat in games[title]['categories'])
-		{
-			document.getElementById("result").innerHTML += "<a href='categories/"+games[title]['categories'][cat]+".html'>("+games[title]['categories'][cat]+")</a> ";
-		}
-		for (var asp in games[title]['aspects'])
-		{
-			document.getElementById("result").innerHTML += "<a href='aspects/"+games[title]['aspects'][asp]+".html'>["+games[title]['aspects'][asp]+"]</a> ";
-		}
-		document.getElementById("result").innerHTML += "</div></li><br>";
 	}
 
 });
