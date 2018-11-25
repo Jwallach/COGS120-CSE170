49e61a7d21730413e8109ecfd3bfe8278aa55b8b
Jwallach <jw.wallach@sbcglobal.net>
2018-11-24 19:54:27 -0800

    basic navbar js changes

--- js/browse_games_old.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    //game mapping
-    var gameMap = games;
-    
-    for (key in gameMap)
-    {
-        document.getElementById("game_list").innerHTML += "<li><a href=" + gameMap[key]["link"] + ">" + gameMap[key]["title"] + "</a></li>";
-    }
-});
--- js/navbar.js
+++ js/navbar.js
@@ -2,10 +2,10 @@ $( document ).ready(function() {
 
 	if (localStorage.getItem("username") === null)
 	{
-		document.getElementById("profile").innerHTML += "<a class='nav-link' href='login.html'>Login</a>"
+		document.getElementById("profile").innerHTML = "<a class='nav-link' href='login.html'>Login</a>"
 	}
 	else
 	{
-		document.getElementById("profile").innerHTML += "<a class='nav-link' href='profile.html'>Profile</a>"
+		document.getElementById("profile").innerHTML = "<a class='nav-link' href='profile.html'>Profile</a>"
 	}
 });
